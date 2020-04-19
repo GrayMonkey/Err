bb13203806513533d4f58958b54bc0af66663fe7
G <graeme.monk@gmail.com>
2020-04-19 13:49:04 +0100

    Loading Screen added
    Loading screen added to allow for assets to be fully loaded before taking action.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] Image logo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AsynchLoad("main"));
+        logo.CrossFadeAlpha(1.0f, 3.0f, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator AsynchLoad(string scene)
+    {
+        yield return null;
+        Debug.Log("Starting Load...");
+
+        AsyncOperation asyncOp = SceneManager.LoadSceneAsync(scene);
+        asyncOp.allowSceneActivation = false;
+
+        while (!asyncOp.isDone)
+        {
+            //1.0f is reserved for the completion of the activation progress.
+            //Values from 0.9 to 1 (extremes excluded) are never used.
+            float progress = Mathf.Clamp01(asyncOp.progress / 0.9f);
+            Debug.Log("Loading Progress:" + (progress * 100) + "%");
+
+            if(asyncOp.progress == 0.9f)
+                asyncOp.allowSceneActivation = true;
+
+            yield return null;
+        }
+    }
+}
